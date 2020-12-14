6f7f402b23254f2adc3ca5fb415cafb829060fdd
Fernando Angulo Donoso <fangulo82@gmail.com>
2020-12-13 23:22:00 -0800

    conditional to check the status and assign a color to the slot

--- script.js
+++ script.js
@@ -8,49 +8,49 @@ function initPage() {
   // Local storage Array
   const openSchedule = [
     {
-      "time": 9,
-      "note": ""
+      time: 9,
+      note: "",
     },
     {
-      "time": 10,
-      "note": ""
+      time: 10,
+      note: "",
     },
     {
-      "time": 11,
-      "note": ""
+      time: 11,
+      note: "",
     },
     {
-      "time": 12,
-      "note": ""
+      time: 12,
+      note: "",
     },
     {
-      "time": 13,
-      "note": ""
+      time: 13,
+      note: "",
     },
     {
-      "time": 14,
-      "note": ""
+      time: 14,
+      note: "",
     },
     {
-      "time": 15,
-      "note": ""
+      time: 15,
+      note: "",
     },
     {
-      "time": 16,
-      "note": ""
+      time: 16,
+      note: "",
     },
     {
-      "time": 17,
-      "note": ""
-    }
+      time: 17,
+      note: "",
+    },
   ];
 
   const dayPlan = JSON.parse(localStorage.getItem("Schedule")) || openSchedule;
   const timeSlotContainer = document.getElementById("timeSlot");
   timeSlotContainer.innerHTML = "";
 
-   // For loop to generate the time slots for the whole day
-   for (let i = 0; i < dayPlan.length; i++) {
+  // For loop to generate the time slots for the whole day
+  for (let i = 0; i < dayPlan.length; i++) {
     let hourString;
     const hour = 9 + i;
     if (hour < 12) {
@@ -58,7 +58,7 @@ function initPage() {
     } else if (hour === 12) {
       hourString = `${hour} PM`;
     } else {
-      hourString = `${hour - 12} PM`
+      hourString = `${hour - 12} PM`;
     }
     const timeSlot = document.createElement("div");
     timeSlot.setAttribute("class", "row input-group timeSlot");
@@ -72,6 +72,38 @@ function initPage() {
     timeSpan.setAttribute("class", "input-group-text hour");
     timeSpan.innerHTML = hourString;
     timeContainer.append(timeSpan);
+
+    const noteEl = document.createElement("input");
+    noteEl.setAttribute("type", "text");
+    // Compare the time slot with the current time to decide what style it should have
+    if (hour < currentHour) {
+      noteEl.setAttribute("class", "form-control description past");
+    } else if (hour === currentHour) {
+      noteEl.setAttribute("class", "form-control description present");
+    } else {
+      noteEl.setAttribute("class", "form-control description future");
+    }
+    noteEl.setAttribute("placeholder", dayPlan[i].note);
+    timeSlot.append(noteEl);
+
+    const saveContainer = document.createElement("div");
+    saveContainer.setAttribute("class", "input-group-append");
+    timeSlot.append(saveContainer);
+
+    const saveSpan = document.createElement("span");
+    saveSpan.setAttribute("class", "saveBtn");
+    saveContainer.append(saveSpan);
+
+    const saveIcon = document.createElement("i");
+    saveIcon.setAttribute("class", "fas fa-save");
+    // Save button saves note entered by user to local storage
+    saveIcon.addEventListener("click", () => {
+      dayPlan[i].note = noteEl.value;
+      localStorage.setItem("plan", JSON.stringify(dayPlan));
+    });
+    saveSpan.append(saveIcon);
   }
 }
 
+// Calling the function
+initPage();

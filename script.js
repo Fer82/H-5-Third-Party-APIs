6f5efb9003f8b31a950ea39d7e5bcad16aceb434
Fernando Angulo Donoso <fangulo82@gmail.com>
2020-12-13 22:54:18 -0800

    script.js function and array for local storage

---
+++ script.js
@@ -0,0 +1,50 @@
+function initPage() {
+  // Get the current hour of the day
+  const currentHour = parseInt(moment().format("kk"));
+  // Update the page with the current date
+  const currentDayEl = document.getElementById("currentDay");
+  currentDayEl.innerHTML = moment().format("dddd, MMMM Do YYYY");
+
+  // Local storage Array
+  const openSchedule = [
+    {
+      "time": 9,
+      "note": ""
+    },
+    {
+      "time": 10,
+      "note": ""
+    },
+    {
+      "time": 11,
+      "note": ""
+    },
+    {
+      "time": 12,
+      "note": ""
+    },
+    {
+      "time": 13,
+      "note": ""
+    },
+    {
+      "time": 14,
+      "note": ""
+    },
+    {
+      "time": 15,
+      "note": ""
+    },
+    {
+      "time": 16,
+      "note": ""
+    },
+    {
+      "time": 17,
+      "note": ""
+    }
+  ];
+
+  
+}
+
